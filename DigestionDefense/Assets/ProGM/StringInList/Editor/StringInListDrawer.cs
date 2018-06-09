aee06427a4a0184df93b2bd8029f9e56347b220b
Ethan Kennerly <ethankennerly@gmail.com>
2018-06-09 16:39:08 -0700

    String In List Drawer: Guards empty list.

--- DigestionDefense/Assets/ProGM/StringInList/Editor/StringInListDrawer.cs
+++ DigestionDefense/Assets/ProGM/StringInList/Editor/StringInListDrawer.cs
@@ -11,9 +11,18 @@ namespace ProGM
       var stringInList = attribute as StringInList;
       var list = stringInList.List;
       if (property.propertyType == SerializedPropertyType.String) {
+        if (list == null || list.Length == 0) {
+          Debug.LogError(typeof(StringInListDrawer) + ": property=" + property.name + ": List is empty.");
+          EditorGUI.Popup (position, property.displayName, -1, list);
+          return;
+        }
         int index = Mathf.Max (0, Array.IndexOf (list, property.stringValue));
         index = EditorGUI.Popup (position, property.displayName, index, list);
-  
+        if (index < 0 || index >= list.Length) {
+          Debug.LogError(typeof(StringInListDrawer) + ": String '" + property.stringValue +
+            "' not found in list. property=" + property.name + ", ");
+          return;
+        }
         property.stringValue = list [index];
       } else if (property.propertyType == SerializedPropertyType.Integer) {
         property.intValue = EditorGUI.Popup (position, property.displayName, property.intValue, list);
