c5780c7cd5a3bbac4f6a7afbaa843b9d28ee6aeb
Ethan Kennerly <ethankennerly@gmail.com>
2018-06-09 19:52:37 -0700

    Receiver Utils: Tapping card asserts coding to-do.

--- DigestionDefense/Assets/Scripts/Utils/ReceiverUtils.cs
+++ DigestionDefense/Assets/Scripts/Utils/ReceiverUtils.cs
@@ -51,6 +51,7 @@ namespace Finegamedesign.Entitas
         #warning TODO: If candidate has one of the filter components.
         public static bool Filter(ReceiverComponent receiver, GameEntity candidate)
         {
+            DebugUtil.Assert(false, "ReceiverUtils.Filter: TODO: If candidate has one of the filter components.");
             return false;
         }
 
@@ -108,6 +109,7 @@ namespace Finegamedesign.Entitas
                 {
                     occupant = context.CreateEntity();
                     #warning TODO: Add component of first name in receiver filter.
+                    DebugUtil.Assert(false, "ReceiverUtils.OccupyIfEmpty: TODO: Add component of first name in receiver filter.");
                 }
 
                 ReplaceOccupant(receiver, occupant);
