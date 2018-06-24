b86b61cfe4add7f0d83dccd1da1d26418c103a37
Ethan Kennerly <ethankennerly@gmail.com>
2018-06-24 16:12:58 -0700

    Nav: Moves namespace to "Finegamedesign.Nav".

---
+++ DigestionDefense/Assets/Scripts/Nav/NavTilemapAgent.cs
@@ -0,0 +1,224 @@
+using System;
+using System.Collections.Generic;
+using SettlersEngine;
+using UnityEngine;
+
+namespace Finegamedesign.Nav
+{
+    [Serializable]
+    public sealed class NavTilemapAgent
+    {
+        [SerializeField]
+        private bool m_AllowsDiagonals = true;
+
+        public bool allowsDiagonals
+        {
+            get
+            {
+                return m_AllowsDiagonals;
+            }
+            set
+            {
+                m_AllowsDiagonals = value;
+                if (m_Solver == null)
+                {
+                    return;
+                }
+                CreateSolver();
+            }
+        }
+
+        private Vector3 m_Destination;
+
+        // Does not interrupt a tween of a step.
+        public Vector3 destination
+        {
+            get
+            {
+                return m_Destination;
+            }
+            set
+            {
+                m_Destination = value;
+                SetDestinationCell(m_Destination);
+                if (m_Path != null)
+                {
+                    return;
+                }
+                FindPathInWorld();
+            }
+        }
+
+        public event Action<Vector3> onPositionChanged;
+
+        private Vector3 m_Position;
+
+        public Vector3 position
+        {
+            get
+            {
+                return m_Position;
+            }
+            set
+            {
+                if (m_Position == value)
+                {
+                    return;
+                }
+                m_Position = value;
+                SetCurrentCell(value);
+                if (onPositionChanged == null)
+                {
+                    return;
+                }
+                onPositionChanged(value);
+            }
+        }
+
+        private NavTilemapController m_Nav;
+
+        public NavTilemapController nav
+        {
+            get
+            {
+                return m_Nav;
+            }
+            set
+            {
+                m_Nav = value;
+            }
+        }
+
+        private Vector2Int m_CurrentCell = new Vector2Int();
+
+        private Vector2Int m_DestinationCell = new Vector2Int();
+
+        private SpatialAStar<MyPathNode, object> m_Solver;
+
+        private IEnumerable<MyPathNode> m_Path;
+
+        private float m_TweenTime = 0f;
+        private float m_StepDuration = 0.25f;
+        private Vector3 m_PreviousStep;
+        private Vector3 m_NextStep;
+
+        private bool m_IsVerbose = false;
+
+        private void SetCurrentCell(Vector3 positionInWorld)
+        {
+            if (m_Nav == null || m_Nav.tilemap == null)
+            {
+                return;
+            }
+            m_CurrentCell = m_Nav.WorldToGrid(positionInWorld);
+        }
+
+        private void SetDestinationCell(Vector3 destinationInWorld)
+        {
+            if (m_Nav == null || m_Nav.tilemap == null)
+            {
+                return;
+            }
+            m_DestinationCell = m_Nav.WorldToGrid(destinationInWorld);
+        }
+
+        private void FindPathInWorld()
+        {
+            if (m_IsVerbose)
+            {
+                Debug.Log("NavTilemapAgent.FindPath: From "
+                    + m_Position + " to " + m_Destination);
+            }
+            SetCurrentCell(m_Position);
+            FindPath();
+        }
+
+        private void CreateSolver()
+        {
+            m_Solver = new SpatialAStar<MyPathNode, object>(m_Nav.grid);
+            m_Solver.allowsDiagonals = m_AllowsDiagonals;
+            m_PreviousStep = position;
+            m_NextStep = position;
+        }
+
+        private void FindPath()
+        {
+            if (m_Solver == null)
+            {
+                CreateSolver();
+            }
+            m_Path = m_Solver.Search(
+                (Vector2)m_CurrentCell,
+                (Vector2)m_DestinationCell,
+                null);
+            if (m_Path == null)
+            {
+                return;
+            }
+            SetNextStep();
+            if (m_IsVerbose)
+            {
+                Debug.Log("NavTilemapAgent.FindPath: From "
+                    + m_CurrentCell + " to " + m_DestinationCell);
+            }
+        }
+
+        public void Update(float deltaTime)
+        {
+            if (deltaTime <= 0.0f)
+            {
+                return;
+            }
+            if (!TweenStep(deltaTime))
+            {
+                return;
+            }
+            FindPath();
+        }
+
+        private bool TweenStep(float deltaTime)
+        {
+            if (m_Path == null)
+            {
+                return false;
+            }
+            if (deltaTime <= 0.0f)
+            {
+                return false;
+            }
+            position = Vector3.Lerp(m_PreviousStep, m_NextStep, m_TweenTime / m_StepDuration);
+            m_TweenTime += deltaTime;
+            if (m_TweenTime < m_StepDuration)
+            {
+                return false;
+            }
+            m_TweenTime -= m_StepDuration;
+            return true;
+        }
+
+        private void SetNextStep()
+        {
+            int index = 0;
+            MyPathNode nextNode = null;
+            // XXX It would be more efficient if path were a list or an array rather than IEnumerable.
+            foreach (MyPathNode node in m_Path)
+            {
+                if (index == 1)
+                {
+                    nextNode = node;
+                    break;
+                }
+                ++index;
+            }
+            if (nextNode == null)
+            {
+                position = m_NextStep;
+                m_Path = null;
+                return;
+            }
+            Vector2Int cell = new Vector2Int(nextNode.X, nextNode.Y);
+            m_PreviousStep = m_NextStep;
+            m_NextStep = m_Nav.GridToWorld(cell);
+        }
+    }
+}
---
+++ DigestionDefense/Assets/Scripts/Nav/NavTilemapController.cs
@@ -0,0 +1,138 @@
+using System;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Finegamedesign.Nav
+{
+    public sealed class NavTilemapController
+    {
+        public event Action<Vector2> onPassableCreated;
+
+        private int m_TilemapLayerMask;
+
+        private Tilemap m_Tilemap;
+
+        public Tilemap tilemap
+        {
+            get
+            {
+                return m_Tilemap;
+            }
+            set
+            {
+                if (m_Tilemap == value)
+                {
+                    return;
+                }
+                m_Tilemap = value;
+
+                m_TilemapLayerMask = 1 << value.gameObject.layer;
+
+                Setup();
+            }
+        }
+
+        private MyPathNode[,] m_Grid;
+
+        public MyPathNode[,] grid
+        {
+            get
+            {
+                return m_Grid;
+            }
+        }
+
+        private bool m_IsVerbose = false;
+
+        // Offsets from minimum cell bounds, which might be less than zero.
+        // Clamps within bounds.  Otherwise the cell is off the grid.
+        public Vector2Int WorldToGrid(Vector3 positionInWorld)
+        {
+            Vector3Int cell3 = m_Tilemap.WorldToCell(positionInWorld);
+            BoundsInt bounds = m_Tilemap.cellBounds;
+            Vector2Int size = new Vector2Int(bounds.size.x - 1, bounds.size.y - 1);
+            Vector2Int cell = new Vector2Int(cell3.x - bounds.xMin, cell3.y - bounds.yMin);
+            cell.Clamp(Vector2Int.zero, size);
+            return cell;
+        }
+
+        // Returns center of cell in grid.
+        public Vector3 GridToWorld(Vector2Int cell)
+        {
+            BoundsInt bounds = m_Tilemap.cellBounds;
+            Vector3Int cell3 = new Vector3Int(cell.x + bounds.xMin, cell.y + bounds.yMin, 0);
+            Vector3 position = m_Tilemap.CellToWorld(cell3);
+            Vector3 cellSize = m_Tilemap.layoutGrid.cellSize;
+            position.x += 0.5f * cellSize.x;
+            position.y += 0.5f * cellSize.y;
+            return position;
+        }
+
+        private Vector2 GridToWorld2D(int gridX, int gridY)
+        {
+            Vector2Int cell = new Vector2Int(gridX, gridY);
+            Vector3 position = GridToWorld(cell);
+            Vector2 position2D = new Vector2(position.x, position.y);
+            return position2D;
+        }
+
+        private Vector3Int GridToCell(int gridX, int gridY)
+        {
+            BoundsInt bounds = m_Tilemap.cellBounds;
+            Vector3Int cell = new Vector3Int(gridX - bounds.xMin, gridY - bounds.yMin, 0);
+            return cell;
+        }
+
+        private bool IsCollision(int gridX, int gridY)
+        {
+            Vector2 point = GridToWorld2D(gridX, gridY);
+            Collider2D collider = Physics2D.OverlapPoint(point, m_TilemapLayerMask);
+            if (collider == null)
+                return false;
+
+            if (collider is TilemapCollider2D)
+                return true;
+
+            if (collider.isTrigger)
+                return false;
+
+            return true;
+        }
+
+        private void Setup()
+        {
+            m_Grid = ParseGrid(m_Tilemap);
+        }
+
+        private MyPathNode[,] ParseGrid(Tilemap wallTilemap)
+        {
+            int width = wallTilemap.size.x;
+            int height = wallTilemap.size.y;
+            MyPathNode[,] grid = new MyPathNode[width, height];
+            for (int x = 0; x < width; ++x)
+            {
+                for (int y = 0; y < height; ++y)
+                {
+                    bool isWall = IsCollision(x, y);
+                    grid[x, y] = new MyPathNode()
+                    {
+                        IsWall = isWall,
+                        X = x,
+                        Y = y,
+                    };
+                    if (m_IsVerbose && isWall)
+                    {
+                        Debug.Log("NavTilemapController.ParseGrid: ("
+                            + x + ", " + y + ") is a wall.");
+                    }
+                    if (isWall || onPassableCreated == null)
+                    {
+                        continue;
+                    }
+                    onPassableCreated(GridToWorld2D(x, y));
+                }
+            }
+            return grid;
+        }
+    }
+}
